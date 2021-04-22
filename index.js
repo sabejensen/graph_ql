66fdf2d65edb8dd99ac82385503f9430dc76f3ef
seajensen <seajensen@gmail.com>
2021-04-21 23:35:46 -0600

    adding docker

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
 const { ApolloServer, gql } = require('apollo-server');
 const pkdata = require('./pkmn/pkmn.json')
 
+const express = require('express')
+const { graphqlHTTP } = require('express-graphql')
+const { buildSchema } = require('graphql')
+const expressPlayground = require('graphql-playground-middleware-express').default
+
+const { PrismaClient } = require('@prisma/client')
+const prisma = new PrismaClient()
+
 // A schema is a collection of type definitions (hence "typeDefs")
 // that together define the "shape" of queries that are executed against
 // your data.
 const typeDefs = gql`
-  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
-
-  # This "Book" type defines the queryable fields for every book in our data source.
   type Pokemon {
     id: String
     name: String
     classification: String
   }
 
-  # The "Query" type is special: it lists all of the available queries that
-  # clients can execute, along with the return type for each. In this
-  # case, the "books" query returns an array of zero or more Books (defined above).
   type Query {
     pokemon: [Pokemon]
   }
@@ -43,7 +45,6 @@ for(let i = 0; i < pkdata.length; i++) {
         name: pkdata[i].name,
         classification: pkdata[i].classification
     }
-
     pokemon.push(currentPk)
 
 }
@@ -53,15 +54,30 @@ for(let i = 0; i < pkdata.length; i++) {
 // schema. This resolver retrieves books from the "books" array above.
 const resolvers = {
     Query: {
-      pokemon: () => pokemon,
+      pokemon: () => pokemon.id,
     },
 };
+
+console.log('PRISMA ', resolvers)
 
 // The ApolloServer constructor requires two parameters: your schema
 // definition and your set of resolvers.
 const server = new ApolloServer({ typeDefs, resolvers });
 
-// The `listen` method launches a web server.
-server.listen().then(({ url }) => {
-  console.log(`🚀  Server ready at ${url}`);
-});
+const app = express()
+
+app.use(
+    '/graphql',
+    graphqlHTTP((req) => ({
+        schema,
+        rootValue: resolvers,
+    }))
+)
+
+const port = 8080
+
+app.get('/', expressPlayground({ endpoint: '/graphql' }))
+
+app.listen({ port }, () => {
+    console.log(`🚀 Server ready at http://localhost:${port}/graphql`)
+})

3eda1b61093e11ebb326e99521366af032142fc5
seajensen <seajensen@gmail.com>
2021-05-03 06:39:18 -0600

    added mutations

--- index.js
+++ index.js
@@ -21,17 +21,25 @@ const typeDefs = gql`
   type Query {
     findPokemon: Pokemon,
     firstPokemon: Pokemon,
+    manyPokemon: Pokemon,
     createPokemon: Pokemon,
     upsertPokemon: Pokemon,
-    resetPokemon: Pokemon
+    resetPokemon: Pokemon,
+  }
+  type Mutation {
+    deletePokemon(id: String!, autoId: String!): Pokemon
+    addPokemon(name: String!, classification: String!, sprite: String!, id: String!): Pokemon
+    updatePokemon(name: String!, classification: String!, autoId: String!): Pokemon
   }
 `;
 
 let allPk = []
 
 const getPk = async () => {
+  let allPk = []
   let myPk = await prisma.pokemon.findMany()
   allPk.push(myPk)
+  return(allPk)
 }
 
 getPk()
@@ -47,14 +55,24 @@ const resolvers = {
         }))
       },
       firstPokemon: () => {
-        return(prisma.pokemon.findFirst())
+        return(prisma.pokemon.findFirst({
+          orderBy: {
+            id: "desc"
+          }
+        }))
+      },
+      manyPokemon: async () => {
+        const pk = await prisma.pokemon.findMany()
+        console.log(pk)
+        return pk
       },
       createPokemon: () => {
         return(prisma.pokemon.create({
           data: {
             name: 'Squirtle Jr.',
             id: '152',
-            classification: 'The best pokemon'
+            classification: 'The best pokemon',
+            sprite: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/151.png'
           }
         }))
       },
@@ -63,12 +81,52 @@ const resolvers = {
           data: {
             name: 'Squirtle Sr.',
             id: '153',
-            classification: 'The worst pokemon'
+            classification: 'The worst pokemon',
+            sprite: 'google.com'
           }
         }))
       },
       resetPokemon: () => prisma.pokemon.deleteMany({})
     },
+    Mutation: {
+      addPokemon: async (name, classification, sprite, id) => {
+        const newPk = await prisma.pokemon.create({
+          data: {
+            name: classification.name,
+            classification: classification.classification,
+            sprite: classification.sprite,
+            id: classification.id,
+          }
+        })
+        setTimeout(() => {
+          return newPk
+        }, 1000)
+      },
+      deletePokemon: async (id, autoId) => {
+        const deletedPk = await prisma.pokemon.delete({
+          where: {
+            autoId: autoId.autoId
+          }
+        })
+        setTimeout(() => {
+          return deletedPk
+        }, 1000)
+      },
+      updatePokemon: async (name, classification, autoId) => {
+        const updatedPk = await prisma.pokemon.update({
+          where: {
+            autoId: classification.autoId
+          },
+          data: {
+            name: classification.name,
+            classification: classification.classification
+          }
+        })
+        setTimeout(() => {
+          return updatedPk
+        }, 1000)
+      }
+    }
 };
 
 console.log('PRISMA ', prisma)
